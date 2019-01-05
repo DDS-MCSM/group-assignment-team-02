ede0095e2496e714433b6f31db323d8534da8dd4
Marta Perez <martap73@hotmail.com>
2019-01-05 21:43:34 +0100

    Actualización package. Actualización report

--- R/code.R
+++ R/code.R
@@ -19,6 +19,18 @@ if (!require(ggplot2)) (install.packages("ggplot2"))
 library(ggplot2)
 if (!require(tidyr)) (install.packages("tidyr"))
 library(tidyr)
+if (!require(dplyr)) (install.packages("dplyr"))
+library(dplyr)
+if (!require(plotly)) (install.packages("plotly"))
+library(plotly)
+if (!require(plyr)) (install.packages("plyr"))
+library(plyr)
+if (!require(lubridate)) (install.packages("lubridate"))
+library(lubridate)
+if (!require(grid)) (install.packages("grid"))
+library(grid)
+if (!require(gridExtra)) (install.packages("gridExtra"))
+library(gridExtra)
 
 
 #' Read CSV
@@ -133,7 +145,7 @@ Plot_pie_Max<-function(x)
   dfMax <- x
   Malware <- dfMax$Malware
 
-  library(plotly)
+
   plot_ly(dfMax, labels = ~Malware, values = ~pctMax, type = 'pie',
                textposition = 'inside',
                textinfo = 'label+percent',
@@ -149,22 +161,22 @@ Plot_pie_Max<-function(x)
            yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
 }
 
-bar_plot <- function(x)
+SemesterColumnsMalwareTotal <- function(x)
 {
   newColNames <- c("Year", "Month", "Day")
   newCols <- colsplit(x$Date, "-", newColNames)
-  a <- semester(xaux$Date, with_year = FALSE)
+  a <- semester(x$Date, with_year = FALSE)
   xaux <- cbind(x, newCols)
   xaux <- cbind(xaux, Semester=a)
 
-  library(dplyr)
+
   temp1 <- filter(xaux, xaux$Malware %in% c("Locky","Cerber","TeslaCrypt"))
 
   temp_Locky <- filter(temp1, temp1$Malware =="Locky")
   temp_Cerber <- filter(temp1, temp1$Malware =="Cerber")
   temp_TeslaCrypt <- filter(temp1, temp1$Malware =="TeslaCrypt")
 
-  library(plyr)
+
   temp_TeslaCrypt_1 <-temp_TeslaCrypt[order(-temp_TeslaCrypt$Year, -temp_TeslaCrypt$Semester),]
   df_Tesla<-data.frame(table(Malware=as.character(temp_TeslaCrypt_1$Malware),Semester=temp_TeslaCrypt_1$Semester,Year=temp_TeslaCrypt_1$Year),stringsAsFactors=FALSE)
 
@@ -184,26 +196,141 @@ bar_plot <- function(x)
 
   temp_plot<-unite(temp, Date,c(2:3),  sep = "-", remove = FALSE)
 
+  return(temp_plot)
+}
+
+
+
+
+Bar_plot_semester_Locky <- function(x)
+{
+  temp_plot <- x
+  temp_plot_Locky <- temp_plot
+  temp_plot_Locky$Freq[temp_plot_Locky$Malware != "Locky"] <- 0
+  Malware <- temp_plot_Locky$Malware
+  Semestre <- as.character(temp_plot_Locky$Date)
+  Semestre <- factor(Semestre, levels=unique(Semestre))
+  Ocurrencias <-temp_plot_Locky$Freq
+
+  plot_ly(temp_plot_Locky, x = ~Semestre, y = ~Ocurrencias, type = 'bar', name = 'Locky', marker = list(color = 'rgb(49,130,189)')) %>%
+    layout(xaxis = list(title = "Semestre", tickangle = -45),
+           yaxis = list(title = ""),
+           margin = list(b = 100),
+           barmode = 'group',
+           title="LOCKY")
+
+}
+
+Bar_plot_semester_Cerber <- function(x)
+{
+
+  temp_plot_Cerber <- x
+  temp_plot_Cerber$Freq[temp_plot_Cerber$Malware != "Cerber"] <- 0
+  Malware <- temp_plot_Cerber$Malware
+  Semestre <- as.character(temp_plot_Cerber$Date)
+  Semestre <- factor(Semestre, levels=unique(Semestre))
+  Ocurrencias <-temp_plot_Cerber$Freq
+
+  plot_ly(temp_plot_Cerber, x = ~Semestre, y = ~Ocurrencias, type = 'bar', name = 'Cerber', marker = list(color = 'rgb(49,130,0)')) %>%
+    layout(xaxis = list(title = "Semestre", tickangle = -45),
+           yaxis = list(title = ""),
+           margin = list(b = 100),
+           barmode = 'group',
+           title="CERBER")
+}
+
+Bar_plot_semester_Tesla <- function(x)
+{
+
+  temp_plot_Tesla <- x
+  temp_plot_Tesla$Freq[temp_plot_Tesla$Malware != "TeslaCrypt"] <- 0
+  Malware <- temp_plot_Tesla$Malware
+  Semestre <- as.character(temp_plot_Tesla$Date)
+  Semestre <- factor(Semestre, levels=unique(Semestre))
+  Ocurrencias <-temp_plot_Tesla$Freq
+
+  plot_ly(temp_plot_Tesla, x = ~Semestre, y = ~Ocurrencias, type = 'bar', name = 'Tesla', marker = list(color = 'rgb(130,30,0)')) %>%
+    layout(xaxis = list(title = "Semestre", tickangle = -45),
+           yaxis = list(title = ""),
+           margin = list(b = 100),
+           barmode = 'group',
+           title="TESLACRYPT")
+}
+
+
+Bar_plot_year <- function(x,year)
+{
+
+  newColNames <- c("Year", "Month", "Day")
+  newCols <- colsplit(x$Date, "-", newColNames)
+  a <- semester(x$Date, with_year = FALSE)
+  xaux <- cbind(x, newCols)
+  xaux <- cbind(xaux, Semester=a)
+
+  xaux <-xaux[order(-xaux$Year, -xaux$Semester),]
+  xaux<-data.frame(table(Malware=as.character(xaux$Malware),Semester=xaux$Semester,Year=xaux$Year),stringsAsFactors=FALSE)
+
+  #Montamos un único dataframe con los tres parciales
+  xaux <-xaux[order(-xaux$Year, -xaux$Semester),]
+
+  #Nos quedamos solo con las del último año (primer y segundo semestre 2018)
+  xaux <- filter(xaux, xaux$Year==year)
+
+
+  #Unimos las columnas Semester y Year para dibujar
+
+  temp_plot<-unite(xaux, Date,c(2:3),  sep = "-", remove = FALSE)
+  temp_plot <- subset(temp_plot, temp_plot$Freq>0)
+
   Malware <- temp_plot$Malware
   Semestre <- as.character(temp_plot$Date)
   Semestre <- factor(Semestre, levels=unique(Semestre))
   Ocurrencias <-temp_plot$Freq
+  titulo <- paste("Year", year," ")
 
-  g = ggplot(temp_plot, aes(x=Semestre, y=temp_plot$Freq, fill=Malware) ) +
-    labs(title = "Sin escala")+ylab("") +
-    theme(plot.title = element_text(size = rel(1), colour = "darkblue"))
-  g=g+geom_bar(position="dodge",stat="identity") + scale_fill_manual(values = alpha(c("orange", "blue","red"), 1)) +
-    theme(axis.title.x = element_text(face="bold", size=10))
 
-  g_escalado = ggplot(temp_plot, aes(x=Semestre, y=temp_plot$Freq, fill=Malware) ) +
-    labs(title = "Escalado")+ylab("") +
-    theme(plot.title = element_text(size = rel(1), colour = "darkblue"))
-  g_escalado=g_escalado+geom_bar(position="dodge",stat="identity") + scale_fill_manual(values = alpha(c("orange", "blue","red"), 1)) +
-    theme(axis.title.x = element_text(face="bold", size=10)) + coord_cartesian(ylim = c(0, 1500))
-
-  library(grid)
-  grid.arrange(g, g_escalado, nrow=1, ncol=2, top=textGrob("Nº Malware semestres 2015-2018", gp=gpar(col="darkblue",fontface="bold")))
+  plot_ly(temp_plot, x = ~Semestre, y = ~Ocurrencias, type = 'bar',split=~Malware) %>%
+    layout(title = titulo)
 }
+
+Bar_plot_year_scale <- function(x,year,yMax)
+{
+
+  newColNames <- c("Year", "Month", "Day")
+  newCols <- colsplit(x$Date, "-", newColNames)
+  a <- semester(x$Date, with_year = FALSE)
+  xaux <- cbind(x, newCols)
+  xaux <- cbind(xaux, Semester=a)
+
+  xaux <-xaux[order(-xaux$Year, -xaux$Semester),]
+  xaux<-data.frame(table(Malware=as.character(xaux$Malware),Semester=xaux$Semester,Year=xaux$Year),stringsAsFactors=FALSE)
+
+  #Montamos un único dataframe con los tres parciales
+  xaux <-xaux[order(-xaux$Year, -xaux$Semester),]
+
+  #Nos quedamos solo con las del último año (primer y segundo semestre 2018)
+  xaux <- filter(xaux, xaux$Year==year)
+
+
+  #Unimos las columnas Semester y Year para dibujar
+
+  temp_plot<-unite(xaux, Date,c(2:3),  sep = "-", remove = FALSE)
+  temp_plot <- subset(temp_plot, temp_plot$Freq>0)
+  temp_plot <- subset(temp_plot, temp_plot$Freq<yMax)
+
+  Malware <- temp_plot$Malware
+  Semestre <- as.character(temp_plot$Date)
+  Semestre <- factor(Semestre, levels=unique(Semestre))
+  Ocurrencias <-temp_plot$Freq
+  titulo <- paste("Year", year," ")
+
+
+  plot_ly(temp_plot, x = ~Semestre, y = ~Ocurrencias, type = 'bar',split=~Malware) %>%
+    layout(title = titulo) %>%
+    layout(yaxis = list(range = c(0,yMax)))
+
+}
+
 
 dibjuar <- function()
 {
@@ -266,11 +393,12 @@ dibjuar <- function()
 
 }
 
-#rw<-downloadCSV()
-#rw<-changeColumnName(rw,"X..Firstseen..UTC.","DateHour")
-#rw <- separateDate(rw)
+#df <- downloadCSV()
+#df <- DeleteColumns(df)
+#df <- changeColumnName(df,"X..Firstseen..UTC.","DateHour")
+#df <- separateDate(df)
+#dfsum <- SumColumnsMalwareTotal(df)
+#dfMax <- SumColumnsMalware(dfsum)
+#dfSem <- SemesterColumnsMalwareTotal(df)
 
 
-
-
-#rw <- changeDateFormat(rw,'%Y-%m-%d %H:%M:%S','%Y-%m-%d')
